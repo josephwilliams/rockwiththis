682cf2091b93e27d357527b9daf77afbbf1747a9
josephwilliams <josephwilli12@gmail.com>
2018-09-26 17:06:20 -0700

    added redux-logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,26 @@
 import thunk from 'redux-thunk'
 import appReducer from '../reducers/index'
 import { createStore, applyMiddleware, compose } from 'redux'
+import { createLogger } from 'redux-logger'
+
+import { SET_SONG_PROGRESS } from '../actions'
 
 // const composeEnhancers = compose
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+// redux-logger
+let reduxLogger
+if (process.env.NODE_ENV === 'development') {
+  reduxLogger = createLogger({
+    // prevent logging of SET_SONG_PROGRESS action
+    predicate: (getState, action) => !action.type.includes(SET_SONG_PROGRESS),
+  })
+}
+
 export default function configureStore() {
   const middleware = [
-    thunk
+    thunk,
+    reduxLogger
   ].filter(Boolean)
   const store = createStore(
     appReducer,
